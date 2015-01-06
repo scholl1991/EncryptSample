de0d15f99e1c9935fad788d2c12c12720c54658b
Serg Shulga <scholl1991@gmail.com>
2015-01-06 14:55:00 +0200

    Initial Commit

---
+++ EncryptSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptSample
+//
+//  Created by Serg Shulga on 1/6/15.
+//  Copyright (c) 2015 TecSynt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
